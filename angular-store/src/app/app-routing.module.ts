45e5e3346aaef2da97de3c6cb63d9b3717092552
Lorena Pinzón <lorena5pinzon@gmail.com>
2020-12-29 20:11:36 -0500

    Adding modules and LazyLoading

--- angular-store/src/app/app-routing.module.ts
+++ angular-store/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
@@ -20,7 +19,8 @@ const routes: Routes = [
       },
       {
         path: 'home',
-        component: HomeComponent
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+        // component: HomeComponent
       },
       {
         path: 'products',
@@ -47,7 +47,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
