4563033a095c6312b9868e4e65e7035f8474c66f
Lorena Pinzón <lorena5pinzon@gmail.com>
2021-01-21 20:08:30 -0500

    Add cart item to the header component

--- angular-store/src/app/modules/material/material.module.ts
+++ angular-store/src/app/modules/material/material.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule
   ]
 })
 export class MaterialModule { }
